2ee325b94c885646a9f3973acf0f89b2c6a4111c
anasDH02 <aggassouli@gmail.com>
2026-08-20 19:16:03 +0100

    add api-gateway

---
+++ api-gateway/src/main/java/com/ecommerce/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ecommerce.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author {ANAS DR}
+ **/
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> {})
+                )
+                .build();
+    }
+}
